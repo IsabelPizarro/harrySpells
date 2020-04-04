35d26236a7796b99668a8e86b30c0c106d46a3f7
Isabel <chabelipizarro1@gmail.com>
2020-04-05 00:18:06 +0200

    make build and change name to docs

---
+++ docs/precache-manifest.b9aa77a52d5038ecf4315a995f0c8b83.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "890b7425e3c4c0792c60de8d5d50613a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "647fd35acfb9a694e6da",
+    "url": "./static/css/main.f6ad511e.chunk.css"
+  },
+  {
+    "revision": "144ee9f1400ed5b32577",
+    "url": "./static/js/2.2c86ad19.chunk.js"
+  },
+  {
+    "revision": "647fd35acfb9a694e6da",
+    "url": "./static/js/main.e57ec945.chunk.js"
+  },
+  {
+    "revision": "a4aa3d64f40ae5931dcd",
+    "url": "./static/js/runtime-main.06ab1236.js"
+  }
+]);
